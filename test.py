8e5c77357b2e0ee16246634bd62a3d49051d529c
Tom den Ottelander <tdenottelander@gmail.com>
2019-06-12 14:06:05 +0200

    Extracted parameters in test.py

--- test.py
+++ test.py
@@ -27,8 +27,15 @@ cooling = 'datasets/energycooling_full.dat'
 heating = 'datasets/energyheating_full.dat'
 tower = 'datasets/tower_full.dat'
 
+# Parameters
+population_size = 100
+max_time = 20
+genetic_algorithm = 'DE'  # Choose from 'PSO', 'DE'
+test_size = 0.5
+dataset = boston
+
 # Load regression dataset
-df = pd.read_csv(boston, header=None, delimiter=' ')		# Replace the corresponding string for a certain data-set
+df = pd.read_csv(dataset, header=None, delimiter=' ')		# Replace the corresponding string for a certain data-set
 
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
@@ -37,30 +44,30 @@ X = X.values
 y = y.values
 
 # Take a dataset split
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 # Set fitness function
-fitness_function = SymbolicRegressionFitness( X_train, y_train )
+fitness_function = SymbolicRegressionFitness(X_train, y_train)
 
 # Set functions and terminals
-functions = [AddNode(with_weights=True), SubNode(with_weights=True), MulNode(with_weights=True), AnalyticQuotientNode(with_weights=True)]	# chosen function nodes
-terminals = [EphemeralRandomConstantNode(with_weights=True)]	# use one ephemeral random constant node
+functions = [AddNode(with_weights=True), SubNode(with_weights=True), MulNode(with_weights=True), AnalyticQuotientNode(with_weights=True)]  # chosen function nodes
+terminals = [EphemeralRandomConstantNode(with_weights=True)]  # use one ephemeral random constant node
 for i in range(X.shape[1]):
-	terminals.append(FeatureNode(i))	# add a feature node for each feature
+	terminals.append(FeatureNode(i))  # add a feature node for each feature
 
 # Run GP
-sgp = SimpleGP(fitness_function, functions, terminals, pop_size=100, max_time=20, genetic_algorithm='DE')	# other parameters are optional
+sgp = SimpleGP(fitness_function, functions, terminals, pop_size=population_size, max_time=max_time, genetic_algorithm=genetic_algorithm)  # other parameters are optional
 sgp.Run()
 
 # Print results
 # Show the evolved function
 final_evolved_function = fitness_function.elite
 nodes_final_evolved_function = final_evolved_function.GetSubtree()
-print ('Function found (',len(nodes_final_evolved_function),'nodes ):\n\t', nodes_final_evolved_function) # this is in Polish notation
+print('Function found (', len(nodes_final_evolved_function),'nodes ):\n\t', nodes_final_evolved_function)  # this is in Polish notation
 # Print results for training set
-print ('Training\n\tMSE:', np.round(final_evolved_function.fitness,3), 
-	'\n\tRsquared:', np.round(1.0 - final_evolved_function.fitness / np.var(y_train),3))
+print('Training\n\tMSE:', np.round(final_evolved_function.fitness, 3),
+	'\n\tRsquared:', np.round(1.0 - final_evolved_function.fitness / np.var(y_train), 3))
 # Re-evaluate the evolved function on the test set
-test_prediction = final_evolved_function.GetOutput( X_test )
-test_mse = np.mean(np.square( y_test - test_prediction ))
-print ('Test:\n\tMSE:', np.round( test_mse, 3), 
-	'\n\tRsquared:', np.round(1.0 - test_mse / np.var(y_test),3))
+test_prediction = final_evolved_function.GetOutput(X_test)
+test_mse = np.mean(np.square(y_test - test_prediction))
+print('Test:\n\tMSE:', np.round(test_mse, 3),
+	'\n\tRsquared:', np.round(1.0 - test_mse / np.var(y_test), 3))
